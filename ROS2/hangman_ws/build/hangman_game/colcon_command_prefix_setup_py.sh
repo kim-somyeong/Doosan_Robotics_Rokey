353c275ac30fa9a572d9d9934fecb4149ec19198
kim-somyeong <ksm001220@naver.com>
2024-10-16 16:30:41 +0900

    commit_commit

---
+++ ROS2/hangman_ws/build/hangman_game/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/somyeong/Doosan_Robotics_Rokey/ROS2/hangman_ws/install/hangman_interfaces/share/hangman_interfaces/package.sh"
---
+++ ROS2/hangman_ws/build/hangman_interfaces/hangman_interfaces__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/somyeong/Doosan_Robotics_Rokey/ROS2/hangman_ws/build/hangman_interfaces/hangman_interfaces__py
+# Build directory: /home/somyeong/Doosan_Robotics_Rokey/ROS2/hangman_ws/build/hangman_interfaces/hangman_interfaces__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
